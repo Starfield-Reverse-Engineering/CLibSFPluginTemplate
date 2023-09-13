135cbdde20ff6aff12e03ee109a3e3e28cb353b6
Angad <angadmisra28@gmail.com>
2023-09-12 22:14:02 -0700

    Plugin.h.in value initialization consistency

--- include/Plugin.h
+++ include/Plugin.h
@@ -4,7 +4,9 @@ using namespace std::string_view_literals;
 
 namespace Plugin
 {
-    static constexpr auto Name    = "PluginName"sv;
-    static constexpr auto Author  = "AuthorName"sv;
-    static constexpr auto Version = REL::Version{ 0, 0, 1, 0 };
+    static constexpr auto Name{ "PluginName"sv };
+    static constexpr auto Author{ "AuthorName"sv };
+    static constexpr auto Version{
+        REL::Version{ 0, 0, 1, 0 }
+    };
 } // namespace Plugin
