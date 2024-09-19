dd6b321c5dfd033b12f7e2649a7b4180c8766cc1
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-09-19 13:03:11 -0700

    Update template

--- include/PCH.h
+++ include/PCH.h
@@ -2,142 +2,111 @@
 
 /* +++++++++++++++++++++++++ C++23 Standard Library +++++++++++++++++++++++++ */
 
-// Concepts library
-#include <concepts>
-
-// Coroutines library
-#include <coroutine>
-
-// Utilities library
-#include <any>
-#include <bitset>
-#include <chrono>
-#include <compare>
+// C standard library headers
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <cinttypes>
+#include <climits>
+#include <clocale>
+#include <cmath>
 #include <csetjmp>
 #include <csignal>
 #include <cstdarg>
 #include <cstddef>
-#include <cstdlib>
-#include <ctime>
-#include <expected>
-#include <functional>
-#include <initializer_list>
-#include <optional>
-#include <source_location>
-#include <tuple>
-#include <type_traits>
-#include <typeindex>
-#include <typeinfo>
-#include <utility>
-#include <variant>
-#include <version>
-
-// Dynamic memory management
-#include <memory>
-#include <memory_resource>
-#include <new>
-#include <scoped_allocator>
-
-// Numeric limits
-#include <cfloat>
-#include <cinttypes>
-#include <climits>
 #include <cstdint>
-#include <limits>
-#include <stdfloat>
-
-// Error handling
-#include <cassert>
-#include <cerrno>
-#include <exception>
-#include <stacktrace>
-#include <stdexcept>
-#include <system_error>
-
-// Strings library
-#include <cctype>
-#include <charconv>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <cuchar>
 #include <cwchar>
 #include <cwctype>
-#include <string>
-#include <string_view>
 
-// Containers library
-#include <array>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <map>
-#include <mdspan>
-#include <queue>
-#include <set>
-#include <span>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-
-// Iterators library
-#include <iterator>
-
-// Ranges library
-#include <ranges>
-
-// Algorithms library
+// C++ standard library headers
 #include <algorithm>
-#include <execution>
-
-// Numerics library
+#include <any>
+#include <array>
+#include <atomic>
+#include <barrier>
 #include <bit>
-#include <cfenv>
-#include <cmath>
+#include <bitset>
+#include <charconv>
+#include <chrono>
+#include <compare>
 #include <complex>
-#include <numbers>
-#include <numeric>
-#include <random>
-#include <ratio>
-#include <valarray>
-
-// Localization library
-#include <clocale>
-#include <locale>
-
-// Input/output library
-#include <cstdio>
+#include <concepts>
+#include <condition_variable>
+#include <coroutine>
+#include <deque>
+#include <exception>
+#include <execution>
+#include <expected>
+#include <filesystem>
+#include <format>
+#include <forward_list>
 #include <fstream>
+#include <functional>
+#include <future>
+#include <initializer_list>
 #include <iomanip>
 #include <ios>
 #include <iosfwd>
 #include <iostream>
 #include <istream>
+#include <iterator>
+#include <latch>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <mdspan>
+#include <memory>
+#include <memory_resource>
+#include <mutex>
+#include <new>
+#include <numbers>
+#include <numeric>
+#include <optional>
 #include <ostream>
 #include <print>
+#include <queue>
+#include <random>
+#include <ranges>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <semaphore>
+#include <set>
+#include <shared_mutex>
+#include <source_location>
+#include <span>
 #include <spanstream>
 #include <sstream>
-#include <streambuf>
-#include <syncstream>
-
-// Filesystem library
-#include <filesystem>
-
-// Regular Expressions library
-#include <regex>
-
-// Atomic Operations library
-#include <atomic>
-
-// Thread support library
-#include <barrier>
-#include <condition_variable>
-#include <future>
-#include <latch>
-#include <mutex>
-#include <semaphore>
-#include <shared_mutex>
+#include <stack>
+#include <stacktrace>
+#include <stdexcept>
+#include <stdfloat>
 #include <stop_token>
+#include <streambuf>
+#include <string>
+#include <string_view>
+#include <syncstream>
+#include <system_error>
 #include <thread>
+#include <tuple>
+#include <type_traits>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <variant>
+#include <vector>
+#include <version>
 
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
@@ -152,6 +121,15 @@ using namespace REL::literals;
 
 namespace logger = SFSE::log;
 namespace stl    = SFSE::stl;
+
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using i8  = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
 
 template <typename T>
 class Singleton
--- project_setup.py
+++ project_setup.py
@@ -1,115 +1,63 @@
-import os
-import re
-import subprocess
-import json
-import shutil
 import stat
+from os import W_OK, access, chmod, environ, remove, rename
+from os.path import isdir, join
+from shutil import rmtree
+from subprocess import run
 
 
 def onexc(func, path, exc_info):
-    if not os.access(path, os.W_OK):
-        os.chmod(path, stat.S_IWUSR)
+    if not access(path, W_OK):
+        chmod(path, stat.S_IWUSR)
         func(path)
     else:
         raise
 
 
 # Remove original git repo
-if os.path.isdir(".git"):
-    shutil.rmtree(".git", onexc=onexc)
+if isdir(".git"):
+    rmtree(".git", onexc=onexc)
 
 # Remove README.md
-os.remove("README.md")
+remove("README.md")
 
 # Get project name and author
 project_name = input("Enter project name: ")
 author = input("Enter author: ")
 print()
 
-# Choose how to consume CommonLibSSE-NG
-from_path = input("Use CommonLibSF from path? (Y/n): ")
-if from_path == "" or from_path.lower() == "y":
-    print(f"Using CommonLibSF from path {os.environ["CommonLibSFPath"]}")
-elif from_path.lower() == "n":
-    print("Using CommonLibSF as submodule")
-else:
-    print("Invalid input")
-    exit()
-
-# Update vcpkg.json
-pattern = re.compile(r"(?<!^)(?=[A-Z])")
-
-with open("vcpkg.json", "r", encoding="utf-8") as vcpkg_json_file:
-    vcpkg_json = json.load(vcpkg_json_file)
-
-name = pattern.sub("-", project_name).lower()
-vcpkg_json["name"] = name
-vcpkg_json["version-semver"] = "1.0.0"
-
-with open("vcpkg.json", "w", encoding="utf-8") as vcpkg_json_file:
-    json.dump(vcpkg_json, vcpkg_json_file, indent=2)
-
 # Update CMakeLists.txt
-with open("CMakeLists.txt", "r", encoding="utf-8") as cmakelists_file:
-    cmakelists = cmakelists_file.read()
+with open("CMakeLists.txt", "r", encoding="utf-8") as f:
+    cmakelists = f.read()
 
 cmakelists = cmakelists.replace("PluginName", project_name)
 cmakelists = cmakelists.replace("AuthorName", author)
 cmakelists = cmakelists.replace("0.0.1", "1.0.0")
 
-if from_path.lower() == "n":
-    cmakelists = cmakelists.replace(
-        "add_subdirectory($ENV{CommonLibSFPath} CommonLibSF)",
-        "add_subdirectory(extern/CommonLibSF)",
-    )
-    cmakelists = cmakelists.replace("$ENV{CommonLibSFPath}", "extern/CommonLibSF")
-
-with open("CMakeLists.txt", "w", encoding="utf-8") as cmakelists_file:
-    cmakelists_file.write(cmakelists)
+with open("CMakeLists.txt", "w", encoding="utf-8") as f:
+    f.write(cmakelists)
 
 # Rename ini file
-os.rename(
-    os.path.join("contrib", "Config", "PluginName.ini"),
-    os.path.join("contrib", "Config", f"{project_name}.ini"),
+rename(
+    join("contrib", "Config", "PluginName.ini"),
+    join("contrib", "Config", f"{project_name}.ini"),
 )
 
 # Update Settings.cpp
-with open(
-    os.path.join("src", "Settings.cpp"), "r", encoding="utf-8"
-) as settings_cpp_file:
-    settings_cpp = settings_cpp_file.read()
+with open(join("src", "Settings.cpp"), "r", encoding="utf-8") as f:
+    settings_cpp = f.read()
 
 settings_cpp = settings_cpp.replace("PluginName.ini", f"{project_name}.ini")
 
-with open(
-    os.path.join("src", "Settings.cpp"), "w", encoding="utf-8"
-) as settings_cpp_file:
-    settings_cpp_file.write(settings_cpp)
+with open(join("src", "Settings.cpp"), "w", encoding="utf-8") as f:
+    f.write(settings_cpp)
 
-# Update vcpkg.json builtin-baseline
-print("Updating vcpkg.json...")
-subprocess.run(
-    [f"{os.environ["VCPKG_ROOT"]}\\vcpkg.exe", "x-update-baseline"], shell=True
-)
+# Update baselines
+print("Updating vcpkg.json baselines...")
+run([f"{environ["VCPKG_ROOT"]}/vcpkg.exe", "x-update-baseline"])
 print()
 
 # Initialize empty git repo
-subprocess.run(["git", "init"])
-
-# Initialize CommonLibSF submodule if chosen
-if from_path.lower() == "n":
-    print("\nInitializing CommonLibSF submodule...")
-    subprocess.run(
-        [
-            "git",
-            "submodule",
-            "add",
-            "https://github.com/Starfield-Reverse-Engineering/CommonLibSF",
-            "extern/CommonLibSF",
-        ]
-    )
-
-    subprocess.run(["git", "submodule", "update", "--init", "--recursive"])
+run(["git", "init"])
 
 # Self-destruct
-os.remove(__file__)
+remove(__file__)
